e9ca23b5b63e8887c0fac1f7983f4495886215be
mgihuac <mgmacfodip@gmail.com>
2017-04-19 17:06:30 +0400

    Added class RotationCode

---
+++ src/paket/RotationCode.java
@@ -0,0 +1,16 @@
+package paket;
+
+/**
+ * Created by korobovka on 19.04.2017.
+ */
+public class RotationCode {
+    public static String encrypt(String str, int shiftCnt) {
+        return "";
+    }
+    public static String decrypt(String str, int shiftCnt) {
+        return "";
+    }
+}
+
+
+
--- src/paket/TrainingSample.java
+++ src/paket/TrainingSample.java
@@ -6,8 +6,8 @@ package paket;
 public class TrainingSample {
     public static void main(String[] args) {
         class Tst4Methods {
-            final int cpBits = 21;
-            // System.out.println(String.format("%04d", Integer.parseInt(Integer.toBinaryString(i))));
+            final int cpBits = 21; // codepoint bits quantity
+            // System.out.println(String.format("%04d", Integ  er.parseInt(Integer.toBinaryString(i))));
             // You'd better change hard-coded "%04d" to "%0" + n + "d"
             //  StringBuilder sb = new StringBuilder();
             // http://stackoverflow.com/questions/31365984/java-integer-parseint-for-32-bit-signed-binary-string-throws-numberformatexcep
@@ -25,15 +25,38 @@ public class TrainingSample {
                 highPart = (arg2shift << (shiftCnt % cpBits)) & 0x1F_FF_FF;
                 return (lowPart | highPart)&0x1F_FF_FF;
             }
+            /**
+             *  cycle shift rightonly 21 letter bits
+             * @param arg2shift
+             * @param shiftCnt
+             * @return arg2shift with 21 letter bits rotated right on shiftCnt
+             */
+            int RORCodePoint(int arg2shift, int shiftCnt) {
+                return ROLCodePoint(arg2shift, (cpBits - shiftCnt % cpBits ));
+            }
+            //int[] codePoints = str.codePoints().toArray();
+            /**
+             *
+             * @param str - string to encrypt
+             * @return encrypted string
+             */
+            String Encrypt(String str) {
+                return "";
+            }
         }
+
         Tst4Methods tstEx = new Tst4Methods();
-        int i = 5;
-        i = 0x1E_EE_EE;
+        int i = 512;
+        //int i = 0x1E_EE_EE;
+        int shiftN = 8;
         //System.out.println(String.format("%032d", Integer.parseInt(Integer.toBinaryString(i))));
-        System.out.println(i);
-        System.out.println(String.format("%1$32s", Integer.toBinaryString(i).replace(" ", "0")));
-        i = tstEx.ROLCodePoint(i,2);
-        System.out.println(String.format("%1$32s", Integer.toBinaryString(i).replace(" ", "0")));
-        System.out.println(i);
+        System.out.println("Original i = " + i);
+        System.out.println(String.format("%1$32s", Integer.toBinaryString(i)).replace(" ", "0"));
+        i = tstEx.ROLCodePoint(i,shiftN);
+        System.out.println(String.format("%1$32s", Integer.toBinaryString(i)).replace(" ", "0"));
+        System.out.println("Left Shifted \"i\" = " + i);
+        //System.out.println(Math.pow(2,17));
+        i = tstEx.RORCodePoint(i,shiftN);
+        System.out.println("Right Shifted(original) \"i\" = "  + i);
     }
 }
